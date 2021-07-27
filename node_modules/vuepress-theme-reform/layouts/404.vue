791ae2b4c94c226645200d0be5322cd9399e68a2
zhaoqingfeng <184868079@qq.com>
2021-07-27 11:24:07 +0800

    添加项目

---
+++ node_modules/vuepress-theme-reform/layouts/404.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="theme-container">
+    <div class="content">
+      <h1>404</h1>
+      <blockquote>{{ getMsg() }}</blockquote>
+      <router-link to="/">回到亲爱的首页</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+const msgs = [
+  `路从哪来，它就在哪`,
+  `我们怎么来到这里的？`,
+  `哪怕有一粒花生米，你也不至于醉得找不到路了。`
+];
+
+export default {
+  methods: {
+    getMsg() {
+      return msgs[Math.floor(Math.random() * msgs.length)];
+    }
+  }
+};
+</script>
